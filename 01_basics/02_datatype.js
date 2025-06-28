3a9517475abe90931078ab69704fe6a82f83b59f
louvkrishnaupadhyay <louv2k@gmail.com>
2025-06-28 18:20:55 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+let age = 3 
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => empty
+undefined
+
+object
+*/
+
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof age)
