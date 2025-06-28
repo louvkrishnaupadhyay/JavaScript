dd8d17f3de286f683e1f2f3d822120a38b4948f1
louvkrishnaupadhyay <louv2k@gmail.com>
2025-06-28 18:10:50 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 123456
+let accountName = "louv krishna"
+var accountPassword = "password"
+accountState = "U.P."
+let accountDetail
+
+// accountId = 3
+// console.log(accountId)
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountName, accountPassword, accountState, accountDetail])
