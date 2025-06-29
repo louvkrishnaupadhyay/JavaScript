f599aa46ccda3c9715a3bcd15cda7ea00a1f05a2
louvkrishnaupadhyay <louv2k@gmail.com>
2025-06-29 19:40:26 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,40 @@
+let myDate = new Date()
+
+// new Date() → Returns a Date object (e.g., 2024-06-28T12:34:56.789Z).
+
+// Date() (without new) → Returns a string (e.g., "Fri Jun 28 2024 14:34:56 GMT+0200").
+
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.toTimeString())
+
+console.log(typeof myDate)            // DATE IS AN OBJECT
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
