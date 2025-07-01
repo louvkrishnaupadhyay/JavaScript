21fb2b87d9ee539b5114e8ab07f0d9f888fb4236
louvkrishnaupadhyay <louv2k@gmail.com>
2025-07-01 13:30:42 +0000

    switch case

---
+++ 04_control_flow.js/switch.js
@@ -0,0 +1,35 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+//ESME EK DIKKAT HOTI HAI JB KOI CASE SATISFY HO JATA HAI TO USKE BAD KE SARE CASE RUN HO JATE HAI 
+//SO ESKO ROKNE KE LIYE HM BREAK KA USE KRTE HAI BUT DEFAULT NAHI RUN HOTA
+
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
